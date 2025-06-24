1f91580cc5437d851e5872f7d13eb669331ce02e
Sulthan M <sulthanmamusa@gmail.com>
2025-06-25 00:20:07 +0530

    added ChromeHeadlessNoSandbox for testing purpose

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,20 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    // browsers: ['Chrome'],
+browsers: ['ChromeHeadlessNoSandbox'],
+customLaunchers: {
+  ChromeHeadlessNoSandbox: {
+    base: 'ChromeHeadless',
+    flags: [
+      '--no-sandbox',
+      '--disable-gpu',
+      '--headless',
+      '--disable-dev-shm-usage',
+      '--remote-debugging-port=9222'
+    ]
+  }
+},
     singleRun: false,
     restartOnFileChange: true
   });
